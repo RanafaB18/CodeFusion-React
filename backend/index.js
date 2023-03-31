28d6bbcbfe8606dc015b028921376796b158ea97
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-03-31 00:07:02 +0000

    I managed to isolate the rooms, now only members of a room will get any messages...very important milestone

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const {roomRouter, roomLinks} = require('./routes/room')
+const http = require('http')
+const server = http.createServer(app)
+const cors = require('cors')
+const { Server } = require('socket.io')
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
+app.use(express.json())
+
+app.use(cors())
+console.log("roomLinks", roomLinks)
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on("join_room", (room) => {
+        console.log(room, "made")
+        socket.join(room)
+    })
+
+    socket.on("send_message", (data) => {
+        console.log(data.room)
+        socket.to(data.room).emit("receive_message", data)
+    })
+})
+
+app.use('/room', roomRouter)
+
+
+const PORT = process.env.PORT
+server.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
+})
---
+++ backend/routes/room.js
@@ -0,0 +1,21 @@
+const shortUUID = require('short-uuid')
+
+const roomRouter = require('express').Router()
+const roomLinks = []
+
+roomRouter.post('/', async (request, response) => {
+    const body = request.body
+    const room = body.room
+    const id = shortUUID.generate()
+    const roomLink = `${room}-${id}`
+    roomLinks.push(roomLink)
+    response.json({roomLink: roomLink})
+})
+
+roomRouter.get('/allRooms', (request, response) => {
+    console.log("Hello")
+    console.log(roomLinks)
+    response.json({ rooms: roomLinks})
+})
+
+module.exports = { roomRouter, roomLinks }
