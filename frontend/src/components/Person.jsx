9b47fcaa78f6a1df39d38590ee572b91fb4269cc
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-04-27 08:09:46 +0000

    Rounded div for initials will now be a perfect sphere/circle

--- frontend/src/components/Person.jsx
+++ frontend/src/components/Person.jsx
@@ -3,10 +3,10 @@ const Person = ({ name }) => {
     <div className="mx-4 my-2 flex items-center">
       <div
         className="
+        flex items-center justify-center
             bg-blacklike
             border-2 border-red-600
-            text-white rounded-full
-            py-2 px-4"
+            text-white rounded-full shrink-0 grow-0 w-11 h-11"
       >
         {name[0].toUpperCase()}
       </div>
