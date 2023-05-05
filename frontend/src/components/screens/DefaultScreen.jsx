638f5d9ba090019c4cdfcb37c4ffb6b2f687ddea
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-05-05 11:39:06 +0000

    Added leave button

--- frontend/src/components/screens/DefaultScreen.jsx
+++ frontend/src/components/screens/DefaultScreen.jsx
@@ -1,15 +1,30 @@
 import { FaEllipsisV } from "react-icons/fa";
 import Options from "../Options";
+import { socket } from "../..";
+import { Link, redirect } from "react-router-dom";
 
-const DefaultScreen = () => {
+const DefaultScreen = ({ username, room }) => {
+  const handleLeave = () => {
+    sessionStorage.clear("user_room_name");
+    socket.emit("leave-room", { room, username });
+    return redirect("/");
+  };
   return (
     <>
       <div className="flex flex-col relative w-full">
-        <div className="h-14 bg-blackhover">
+        <div className="flex justify-between h-14 bg-blackhover">
+          <Link
+            to={"/"}
+            className="bg-red-500
+                text-white text-md
+            font-semibold rounded-md px-4 py-2
+            tracking-wide hover:bg-blue-500 m-2"
+            onClick={handleLeave}
+          >
+            Leave
+          </Link>
           <div
-            className="
-        bg-white h-14 w-16 float-right
-        bg-opacity-20 flex justify-center items-center"
+            className="bg-white h-14 w-16 float-right bg-opacity-20 flex justify-center items-center"
           >
             <FaEllipsisV className="text-white text-lg cursor-pointer" />
           </div>
@@ -22,4 +37,4 @@ const DefaultScreen = () => {
   );
 };
 
-export default DefaultScreen
+export default DefaultScreen;
