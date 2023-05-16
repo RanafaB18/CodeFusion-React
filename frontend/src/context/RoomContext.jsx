02fc5f6ca0baf44e354d4525e6f632a041f21e1e
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-05-16 23:52:50 +0000

    Updated cors

--- frontend/src/context/RoomContext.jsx
+++ frontend/src/context/RoomContext.jsx
@@ -6,7 +6,7 @@ import { peerReducer } from "./peerReducer";
 import { addPeerAction, removePeerAction } from "./peerActions";
 
 export const RoomContext = createContext(null);
-const socket = io("https://code-fusion-react.vercel.app/", { transports : ['polling'] });
+const socket = io("https://code-fusion-react.vercel.app/", { transports : ['websocket'] });
 
 export const RoomProvider = ({ children }) => {
   const [me, setMe] = useState();
