105571ac437b1757311e3c3c6c255048492093c6
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-05-16 21:02:46 +0000

    Changing backend link

--- frontend/src/context/RoomContext.jsx
+++ frontend/src/context/RoomContext.jsx
@@ -6,7 +6,7 @@ import { peerReducer } from "./peerReducer";
 import { addPeerAction, removePeerAction } from "./peerActions";
 
 export const RoomContext = createContext(null);
-const socket = io("http://localhost:3004");
+const socket = io("https://code-fusion-react.vercel.app");
 
 export const RoomProvider = ({ children }) => {
   const [me, setMe] = useState();
