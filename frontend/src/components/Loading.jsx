28d8ddff3fe341113ddbe1c8f4d24fa51bd11ff6
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-05-22 09:19:21 +0000

    Centered loading screen on large screens too

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,8 +3,8 @@ import loadingAnimation from "../assets/98288-loading.json"
 
 const Loading = () => {
     return (
-        <div className="bg-blackish w-screen h-screen">
-            <Lottie animationData={loadingAnimation} loop={true} />
+        <div className="bg-blackish w-screen h-screen relative">
+            <Lottie animationData={loadingAnimation} className="absolute inset-0 m-auto" loop={true} />
         </div>
     )
 }
