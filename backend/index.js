18f98230b8239d47d2f47acabf6cee4ea236b3ab
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-07-22 10:42:22 +0000

    Added fallback port

--- backend/index.js
+++ backend/index.js
@@ -226,7 +226,7 @@ app.post('/:room/users', (req, res) => {
     return res.end()
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
 })
